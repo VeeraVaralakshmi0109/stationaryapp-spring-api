563e821a636371be7964b31545e1bc705bc80862
VeeraVaralakshmi0109 <sallavaralakshmi.99@gmail.com>
2022-04-27 17:03:49 +0530

    Added empty project

---
+++ src/main/java/com/stationaryapp/stationaryappapi/StationaryappApiApplication.java
@@ -0,0 +1,13 @@
+package com.stationaryapp.stationaryappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationaryappApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationaryappApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stationaryapp/stationaryappapi/StationaryappApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stationaryapp.stationaryappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StationaryappApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
